108bda1b428d9f5f6d4b59d7ad4c047cf8914160
ElenaPupysheva <nyankem@gmail.com>
2024-06-26 19:49:28 +0300

    Выполнение первого проекта

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,33 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 public class Main {
-
     public static void main(String[] args) {
         // ваш код начнется здесь
         // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-        System.out.println("Привет Мир");
-    }
+        System.out.println("Добро пожаловать на гонку 24 часа Ле-Мана! Давайте зарегестрируем 3 участников!");
+        ArrayList<Car> carList = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        for (int i=1; i<4;i++){
+            System.out.println("Введите марку машины №:"+i);
+            String name = scanner.next();
+            System.out.println("Введите скорость машины:"+i);
+            int speed;
+            while (true) {
+                speed = scanner.nextInt();
+                if ((speed > 0) && (speed <= 250)) {
+                    break;
+                } else {
+                    System.out.println("Неправильная скорость");
+                    System.out.println("Введите скорость машины:" + i);
+                }
+
+            }
+            carList.add(new Car(name,speed));
+            Race.getWinner(name,speed);
+
+        }
+        System.out.println("Гонка завершена!");
+        for (Car car : carList) System.out.println("В гонке участвовали: "+car.name+" со скоростью " +car.speed+ " км/ч");
+        System.out.println("Ура победителю гонки Ле-Мана "+Race.winnerName);
+        }
 }
---
+++ src/main/java/Race.java
@@ -0,0 +1,14 @@
+public class Race {
+    static String winnerName = "";
+    static int hours = 24;
+    static int distance = 0;
+    public static String getWinner(String name, int speed) {
+        int newDistance = speed*hours;
+        if (distance < newDistance){
+            distance=speed*hours;
+            winnerName = name;
+        }
+
+        return winnerName;
+    }
+}
